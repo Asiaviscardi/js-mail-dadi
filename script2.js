0533cff20fd2557062d3dfd712085cd73e9a4aa3
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-13 17:51:45 +0200

    termine esercizio con modifiche

--- script2.js
+++ script2.js
@@ -1,16 +1,14 @@
-const numeri = ["1", "2", "3", "4", "5", "6" ]
-
-const scelta = prompt ("scegli un numero")
-
 const numeropc = Math.ceil(Math.random() * 6);
+
+const numerogiocatore = Math.ceil(Math.random() * 6);
 
 console.log(numeropc)
 
-if (scelta > numeropc){
+if (numerogiocatore > numeropc){
     console.log("il vingitore è umano")
   }
 
-  else if(scelta < numeropc){
+  else if(numerogiocatore < numeropc){
     console.log("Il vincitore è PC")
   }
   
